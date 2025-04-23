3be168bbb662d243cb5725322ecae2a19c16050c
A2kmoise <abayomoise950@gmail.com>
2025-04-23 16:34:26 +0200

    url-shortner 1.0.0

--- server-app/src/server.ts
+++ server-app/src/server.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger/swagger.json';
 import connectDb from './config/dbConfig';
 import shortUrl from './routes/shortUrl'
 
 dotenv.config();
 connectDb();
 
-
+      
 const port = process.env.PORT || 5001;
 const app = express();
 app.use(express.json());
@@ -19,6 +21,7 @@ app.use(cors({
 );
 
 app.use ( "/api/", shortUrl);
+app.use ('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.listen(port, () => {
     console.log(`Server started successfully at port ${port}`);
 });
